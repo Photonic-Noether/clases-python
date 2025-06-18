80ebe6f881d8a26033ab46a3aa6a58d5bdf7193b
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2025-06-18 18:23:10 +0200

    Ejercicios interesantes añadidos

---
+++ ejercicios.py
@@ -0,0 +1,16 @@
+# Ejercicios interesantes
+
+# 1 - ISBN10 to ISBN13 converter: https://www.codewars.com/kata/61ce25e92ca4fb000f689fb0/python
+
+
+
+# 2 - ISBN10 validator: https://www.codewars.com/kata/51fc12de24a9d8cb0e000001/python
+
+
+
+# 3 - Split strings: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/python
+
+
+
+# 4 - Lazy repeater: https://www.codewars.com/kata/51fc3beb41ecc97ee20000c3
+
