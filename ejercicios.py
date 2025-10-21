ac14071f30f01e6a35c088f2d848eb3c274a699e
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2025-10-21 19:37:44 +0200

    Añadidos ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,8 @@
+# Aqui me vais a hacer los ejercicios para mañana
+
+# 1. Un alumno con notas y quiero que me imprima todas sus notas
+
+
+
+# 2. Un diccionario Usuario que tenga nombre de usuario, contraseña y email
+
